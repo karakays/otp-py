d13f8fb5152dfe4f90b15a171bb47d3ca28fbc39
Selçuk Karakayalı <skarakayali@gmail.com>
2018-03-23 10:21:19 +0300

    licensing

--- otp-py/main.py
+++ otp-py/main.py
@@ -1,6 +1,29 @@
+# MIT License
+# 
+# Copyright (c) 2018 Selçuk Karakayalı
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import sys
 import time
 # import logging
+import core
+from urllib.parse import urlparse, unquote
 from token import Token, InvalidTokenUriError
 from progress import Progress, Bar
 
@@ -8,9 +31,11 @@ from progress import Progress, Bar
 
 
 def main():
-    url = sys.argv[1]
+    arg = sys.argv[1]
     try:
-        token = Token.fromString(url)
+        uri = urlparse(unquote(arg))
+
+        token = core.get_otp_by_uri(uri)
 
         token_code = token.generateCode()
 
@@ -23,10 +48,14 @@ def main():
         progress.attach(bar)
 
         # logger.debug('xxx'.format(p, r, token_code.progress))
-        for i in progress.iter(range(int(r * 30))):
-            time.sleep(1)
+        #for i in progress.iter(range(int(r * 30))):
+        #    time.sleep(1)
 
-    except InvalidTokenUriError:
+        qr_code = core.create_qr_code(token)
+        print(qr_code)
+
+    except InvalidTokenUriError as e:
+        print(e)
         pass
         # print('Invalid uri', url, file=sys.stderr)
     except KeyboardInterrupt:
--- otp-py/token.py
+++ otp-py/token.py
@@ -1,14 +1,30 @@
+# MIT License
+# 
+# Copyright (c) 2018 Selçuk Karakayalı
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import time
 import hmac
 import base64
 import hashlib
 import functools
 from enum import Enum
-
-
-def create_otp(secret, counter):
-    token = Token('I2TE4DXFFG4QQKBS', 30, 'SHA1', 6)
-    return token.generateCode()
 
 
 class TokenCode:
@@ -50,16 +66,33 @@ class TokenType(Enum):
             else:
                 return None
 
+        def __str__(self):
+            return self.name.lower()
+
 
 class Token:
-    def __init__(self, secret, period, algorithm, digits):
+    def __init__(self, t_type, issuer, user, secret,
+                 period, algorithm, digits):
+        self.type = t_type
+        self.issuer = issuer
+        self.user = user
         self.secret = secret
         self.period = period
         self.algorithm = algorithm
         self.digits = digits
 
     def toUri(self):
-        pass
+        """
+        otpauh://TYPE/LABEL?PARAMS where
+        LABEL is issuer:user
+        """
+        from urllib.parse import urlunsplit, urlencode
+        query = {'secret': self.secret, 'period': self.period,
+                 'algorithm': self.algorithm, 'digits': self.digits}
+
+        return urlunsplit(('otpauth', self.type.__str__(),
+                           self.issuer + ':' + self.user,
+                           urlencode(query), None))
 
     def generateCode(self):
         start = time.time()
@@ -84,9 +117,20 @@ class Token:
     def fromUri(cls, uri):
         from urllib.parse import parse_qs
 
+        scheme = uri.scheme
+
+        if scheme != 'otpauth':
+            raise InvalidTokenUriError()
+
         token_type = TokenType.fromValue(uri.netloc)
         if not token_type:
             raise InvalidTokenUriError()
+
+        label = uri.path
+        if not label:
+            raise InvalidTokenUriError()
+
+        issuer, user = label[1:].split(':')
 
         params = parse_qs(uri.query)
 
@@ -104,18 +148,40 @@ class Token:
         digits = params.get('digits')
         try:
             digits = 6 if not digits else int(digits[0])
+            if digits not in (6, 7, 8):
+                raise ValueError
+        except ValueError:
+            raise InvalidTokenUriError()
+
+        issuer_q = params.get('issuer')
+        try:
+            if issuer_q:
+                if issuer and issuer_q[0] != issuer:
+                    raise ValueError
+                else:
+                    issuer = issuer_q[0]
         except ValueError:
             raise InvalidTokenUriError()
 
         algorithm = params.get('algorithm')
-        algorithm = 'SHA1' if not algorithm else algorithm[0]
+        try:
+            algorithm = 'SHA1' if not algorithm else algorithm[0]
+            if algorithm not in ('SHA1', 'SHA256', 'SHA512'):
+                raise ValueError
+        except ValueError:
+            raise InvalidTokenUriError()
 
-        return Token(secret, period, algorithm, digits)
+        return Token(token_type, issuer, user, secret,
+                     period, algorithm, digits)
 
     @classmethod
     def fromString(cls, string):
-        from urllib.parse import urlparse
-        return Token.fromUri(urlparse(string))
+        from urllib.parse import urlparse, unquote
+        return Token.fromUri(urlparse(unquote(string)))
+
+    def __str__(self):
+        return 'Token[issuer={}, user={}, secret={}, digits={}, algorithm={}, period={}]'.format(self.issuer, self.user, self.secret,
+                self.digits, self.algorithm, self.period)
 
 
 class InvalidTokenUriError(Exception):
