60dbf7701fe9f726ecc717c494bdfca059bba3cc
Selçuk Karakayalı <skarakayali@gmail.com>
2018-04-06 10:41:07 +0300

    snapshot release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='otp',
-    version='0.1',
+    version='0.1.0.dev1',
     author='Selçuk Karakayalı',
     author_email='skarakayali@gmail.com',
     maintainer='Selçuk Karakayalı',
