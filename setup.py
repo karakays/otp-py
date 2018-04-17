1961839d43d5d07ae115d1360820940225e10392
Selçuk Karakayalı <skarakayali@gmail.com>
2018-04-17 13:31:58 +0300

    version 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='otp-gen',
-    version='0.1.1',
+    version='0.1.2',
     author='Selçuk Karakayalı',
     author_email='skarakayali@gmail.com',
     maintainer='Selçuk Karakayalı',
