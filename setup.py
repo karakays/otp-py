190120c983411e03774f3421f79e1567705ab41b
Selçuk Karakayalı <skarakayali@gmail.com>
2019-04-24 14:51:24 +0300

    update versioning mechanism

--- setup.py
+++ setup.py
@@ -2,19 +2,28 @@
 # -*- coding: utf-8
 from setuptools import setup, find_packages
 
+pkg_vars = {}
+
+with open("otp/_version.py") as fp:
+    exec(fp.read(), pkg_vars)
+
 setup(
     name='otp-gen',
-    version='0.1.2',
+    version=pkg_vars['__version__'],
     author='Selçuk Karakayalı',
     author_email='skarakayali@gmail.com',
     maintainer='Selçuk Karakayalı',
     url='http://github.com/karakays/otp-py/',
     packages=find_packages(),
     install_requires=['pyqrcode>=1.2.1'],
-    python_requires='>=3',
+    python_requires='>=3.6',
     license='MIT',
     keywords=['otp', 'otp-codes', 'otp-generator', 'authenticator'],
     description='Generate one time passwords',
     long_description=open('README.rst').read(),
-    scripts=['bin/otp']
+    scripts=['bin/otp'],
+    classifiers=[ "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License"]
 )
